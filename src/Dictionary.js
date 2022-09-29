ddbeff171ba30f5a5c8240dd82f968cb502d737e
OlenaKut <cundrochka@gmail.com>
2022-09-30 01:49:48 +0200

    Fixed more issues

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,7 +14,6 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     //Documentation https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
